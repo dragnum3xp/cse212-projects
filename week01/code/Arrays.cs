3d8266e31d993e90cda7877db920d0ef7972ca29
Dragnum <dragnumexp@gmail.com>
2026-01-08 12:21:01 -0300

    Finished assignments week 01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -7,13 +7,14 @@ public static class Arrays
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    {   
+        double[] values = new double[length]; //Creates an array with the results
+        for( int i = 0; i < length; i++) // iterate through the length to ensure the loop stops at the right size
+        {   
+            values[i] = number * (i +1); //the logic itself
+        }
 
-        return []; // replace this return statement with your own
+        return values;
     }
 
     /// <summary>
@@ -25,9 +26,12 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        int removedValue;
+        for( int i = 0; i < amount; i++) // iterate through the length to ensure the loop stops at the right size
+        {
+            removedValue = data[data.Count - 1]; // Stores the last value of the list in a variable
+            data.RemoveAt(data.Count - 1); // remove the value
+            data.Insert(0, removedValue); //read the stored value and adds it to the front of the list
+        }
     }
 }
